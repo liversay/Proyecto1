113fbfc1b82da3b5d6fa177934dd3858b063d762
liversay <sespinoc131005@gmail.com>
2024-10-21 21:15:53 -0500

    Interfaz gráfica y asignación de nombres

--- Proyecto1/Form1.Designer.cs
+++ Proyecto1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proyecto1
 {
-    partial class Form1
+    partial class Calculadora
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,31 +28,320 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnPotencia = new Button();
+            textBox1 = new TextBox();
+            btnCE = new Button();
+            btnC = new Button();
+            btnBorrar = new Button();
+            btnDivision = new Button();
+            btnTrigonometrica = new Button();
+            btnLog = new Button();
+            btnRaiz = new Button();
+            btnResta = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btnMultiplicacion = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btnIgual = new Button();
+            btnPunto = new Button();
+            btn0 = new Button();
+            btnHistorial = new Button();
+            btnSuma = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
             SuspendLayout();
             // 
-            // button1
+            // btnPotencia
             // 
-            button1.Location = new Point(122, 57);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnPotencia.Location = new Point(99, 104);
+            btnPotencia.Name = "btnPotencia";
+            btnPotencia.Size = new Size(82, 57);
+            btnPotencia.TabIndex = 0;
+            btnPotencia.Text = "x^y";
+            btnPotencia.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // textBox1
+            // 
+            textBox1.Location = new Point(99, 71);
+            textBox1.Name = "textBox1";
+            textBox1.RightToLeft = RightToLeft.Yes;
+            textBox1.Size = new Size(346, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "0";
+            // 
+            // btnCE
+            // 
+            btnCE.Location = new Point(187, 104);
+            btnCE.Name = "btnCE";
+            btnCE.Size = new Size(82, 57);
+            btnCE.TabIndex = 2;
+            btnCE.Text = "CE";
+            btnCE.UseVisualStyleBackColor = true;
+            // 
+            // btnC
+            // 
+            btnC.Location = new Point(275, 104);
+            btnC.Name = "btnC";
+            btnC.Size = new Size(82, 57);
+            btnC.TabIndex = 3;
+            btnC.Text = "C";
+            btnC.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(363, 104);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(82, 57);
+            btnBorrar.TabIndex = 4;
+            btnBorrar.Text = "Borrar";
+            btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnDivision
+            // 
+            btnDivision.Location = new Point(363, 167);
+            btnDivision.Name = "btnDivision";
+            btnDivision.Size = new Size(82, 57);
+            btnDivision.TabIndex = 8;
+            btnDivision.Text = "/";
+            btnDivision.UseVisualStyleBackColor = true;
+            // 
+            // btnTrigonometrica
+            // 
+            btnTrigonometrica.Location = new Point(275, 167);
+            btnTrigonometrica.Name = "btnTrigonometrica";
+            btnTrigonometrica.Size = new Size(82, 57);
+            btnTrigonometrica.TabIndex = 7;
+            btnTrigonometrica.Text = "trg";
+            btnTrigonometrica.UseVisualStyleBackColor = true;
+            // 
+            // btnLog
+            // 
+            btnLog.Location = new Point(187, 167);
+            btnLog.Name = "btnLog";
+            btnLog.Size = new Size(82, 57);
+            btnLog.TabIndex = 6;
+            btnLog.Text = "log";
+            btnLog.UseVisualStyleBackColor = true;
+            // 
+            // btnRaiz
+            // 
+            btnRaiz.Location = new Point(99, 167);
+            btnRaiz.Name = "btnRaiz";
+            btnRaiz.Size = new Size(82, 57);
+            btnRaiz.TabIndex = 5;
+            btnRaiz.Text = "√x";
+            btnRaiz.UseVisualStyleBackColor = true;
+            // 
+            // btnResta
+            // 
+            btnResta.Location = new Point(363, 293);
+            btnResta.Name = "btnResta";
+            btnResta.Size = new Size(82, 57);
+            btnResta.TabIndex = 16;
+            btnResta.Text = "-";
+            btnResta.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(275, 293);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(82, 57);
+            btn6.TabIndex = 15;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(187, 293);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(82, 57);
+            btn5.TabIndex = 14;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(99, 293);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(82, 57);
+            btn4.TabIndex = 13;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplicacion
+            // 
+            btnMultiplicacion.Location = new Point(363, 230);
+            btnMultiplicacion.Name = "btnMultiplicacion";
+            btnMultiplicacion.Size = new Size(82, 57);
+            btnMultiplicacion.TabIndex = 12;
+            btnMultiplicacion.Text = "*";
+            btnMultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(275, 230);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(82, 57);
+            btn9.TabIndex = 11;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(187, 230);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(82, 57);
+            btn8.TabIndex = 10;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(99, 230);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(82, 57);
+            btn7.TabIndex = 9;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btnIgual
+            // 
+            btnIgual.Location = new Point(363, 419);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(82, 57);
+            btnIgual.TabIndex = 24;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = true;
+            // 
+            // btnPunto
+            // 
+            btnPunto.Location = new Point(275, 419);
+            btnPunto.Name = "btnPunto";
+            btnPunto.Size = new Size(82, 57);
+            btnPunto.TabIndex = 23;
+            btnPunto.Text = ".";
+            btnPunto.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(187, 419);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(82, 57);
+            btn0.TabIndex = 22;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnHistorial
+            // 
+            btnHistorial.Location = new Point(99, 419);
+            btnHistorial.Name = "btnHistorial";
+            btnHistorial.Size = new Size(82, 57);
+            btnHistorial.TabIndex = 21;
+            btnHistorial.Text = "Mostrar Cálculos";
+            btnHistorial.UseVisualStyleBackColor = true;
+            // 
+            // btnSuma
+            // 
+            btnSuma.Location = new Point(363, 356);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(82, 57);
+            btnSuma.TabIndex = 20;
+            btnSuma.Text = "+";
+            btnSuma.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(275, 356);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(82, 57);
+            btn3.TabIndex = 19;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(187, 356);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(82, 57);
+            btn2.TabIndex = 18;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(99, 356);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(82, 57);
+            btn1.TabIndex = 17;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // Calculadora
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
+            ClientSize = new Size(560, 550);
+            Controls.Add(btnIgual);
+            Controls.Add(btnPunto);
+            Controls.Add(btn0);
+            Controls.Add(btnHistorial);
+            Controls.Add(btnSuma);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btnResta);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btnMultiplicacion);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btnDivision);
+            Controls.Add(btnTrigonometrica);
+            Controls.Add(btnLog);
+            Controls.Add(btnRaiz);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnC);
+            Controls.Add(btnCE);
+            Controls.Add(textBox1);
+            Controls.Add(btnPotencia);
+            Name = "Calculadora";
+            Text = "Calculadora";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnPotencia;
+        private TextBox textBox1;
+        private Button btnCE;
+        private Button btnC;
+        private Button btnBorrar;
+        private Button btnDivision;
+        private Button btnTrigonometrica;
+        private Button btnLog;
+        private Button btnRaiz;
+        private Button btnResta;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btnMultiplicacion;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btnIgual;
+        private Button btnPunto;
+        private Button btn0;
+        private Button btnHistorial;
+        private Button btnSuma;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
     }
 }
--- Proyecto1/Form1.cs
+++ Proyecto1/Form1.cs
@@ -1,8 +1,8 @@
 namespace Proyecto1
 {
-    public partial class Form1 : Form
+    public partial class Calculadora : Form
     {
-        public Form1()
+        public Calculadora()
         {
             InitializeComponent();
         }
